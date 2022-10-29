4da894a303b77687ba0cc736023b540eb9284cb5
user <oksi.1213@mail.ru>
2022-10-29 23:27:37 +1000

    A003

---
+++ A003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// пример из 5 элементов:
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+
+Console.WriteLine("Введите число: ");
+string NumbersStr = Console.ReadLine();
+string [] array = NumbersStr.Split();
+Console.Write("[");
+
+for (int i=0; i < array.Length-1; i++)
+
+Console.Write(array[i]+", ");
+
+Console.WriteLine(array[array.Length-1]+"]");
